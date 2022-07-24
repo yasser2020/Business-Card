815d2df095364ce97cedd1d23cfdb099e620138b
FlutLab.io <>
2022-07-24 10:14:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.blueAccent,
             appBar: AppBar(title: Text("Hi Yasser"), backgroundColor: Colors.redAccent),
             body: SafeArea(
-                child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
               Icon(Icons.star, size: 50, color: Colors.yellow),
               Icon(Icons.star, size: 50, color: Colors.yellow),
               Icon(Icons.star, size: 50, color: Colors.yellow),
