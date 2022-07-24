68324e2eaea2ba05f19e8b5e7244c67c08c909d0
FlutLab.io <>
2022-07-24 10:58:22 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
             body: SafeArea(
                 child: Column(
               children: [
-                CircleAvatar(radius: 50, backgroundColor: Colors.white)
+                CircleAvatar(radius: 50, backgroundImage: AssetImage('ronaldo.png'))
               ],
             ))));
   }
