62c4eab0259aee2b30aa00d7de872f09e24e25a1
FlutLab.io <>
2022-07-24 10:25:57 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
             body: SafeArea(
                 child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
               Container(
+                width: 20,
                 color: Colors.yellow,
                 child: Icon(
                   Icons.star,
